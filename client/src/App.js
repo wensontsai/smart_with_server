ad6789f137a769e91adc34d770f2357e5f1faa66
Wenson Tsai <wensontsai@gmail.com>
2018-06-04 22:55:59 -0400

    further setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 
-import logo from './logo.svg';
-
+// Styles
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+// routes
+import routes from './routes';
+
+// common components
+import Header from './common/components/Header'
+import Footer from './common/components/Footer'
+
 class App extends Component {
-  state = {
-    response: ''
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div className="App">
+            <Header />
+            <div className="wrap">
+              {routes}
+            </div>
+            <Footer />
+          </div>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
---
+++ client/src/about/index.js
@@ -0,0 +1,61 @@
+// import React from 'react'
+// import { Container } from 'reactstrap';
+// import './style.css';
+
+// class About extends React.Component {
+
+//   render() {
+
+//     return (
+//     	<div id="about">
+// 	    	<Container>
+// 	    		<h2 className="text-center">
+// 		        About
+// 		      </h2>
+// 		      <p>About page content</p>
+// 			  </Container>
+// 		  </div>
+//     );
+//   }
+
+// }
+
+// export default About;
+
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import './style.css';
+
+class About extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.response}</p>
+      </div>
+    );
+  }
+}
+
+export default About;
