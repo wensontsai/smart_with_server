153ee776c39ce414e0599275bcd0708011aefde3
Wenson Tsai <wensontsai@gmail.com>
2018-06-04 22:44:57 -0400

    first

---
+++ client/ethereum/factory.js
@@ -0,0 +1,12 @@
+import web3 from './web3';
+import ADDRESS from './secrets';
+
+// always need ABI
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+    JSON.parse(CampaignFactory.interface),
+    ADDRESS
+);
+
+export default instance;
---
+++ client/ethereum/secrets.js
@@ -0,0 +1,8 @@
+const mnemonic = "fault boring vintage pottery just myth rib banner town tomorrow weasel parent";
+exports.mnemonic = mnemonic;
+
+const providerLink = "https://rinkeby.infura.io/YJIgYmGN9BvXWzBs25Ma";
+exports.providerLink = providerLink;
+
+const ADDRESS = '0x895654F37f77E6b2F11Db8eEE592c724fdCeBB60';
+exports.ADDRESS = ADDRESS;
